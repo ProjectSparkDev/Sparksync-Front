eea8d4d567d2c05fcba0125729f539fc5adc402c
soltinator <ali.soltani2000@hotmail.com>
2024-12-26 21:14:30 +0330

    added a template for writing  a faq later

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -1,6 +1,24 @@
-<div class="d-flex justify-content-center align-items-center" style="height: 100vh; color: #e4e4e4">
-    <div class="text-center">
-        <h1>FAQ HERE</h1>
-        <p>under construction</p>
-    </div>
+<div class="d-flex justify-content-center align-items-center" style="color: #e4e4e4;">
+	<div class="text-center" style="width: 85%;">
+		<br />
+		<h1>Frequently asked questions</h1>
+		<br />
+
+		
+<!-- question and answer template here -->
+		<div class="accordion" id="accordionExample">
+			<div class="accordion-item">
+				<h2 class="accordion-header">Question #1</h2>
+
+				<div class="accordion-body">
+					<strong>answer of the question number 1</strong> 
+					here we answer them in detail.
+				</div>
+			</div>
+<!-- ---------- -->
+
+
+			<br />
+		</div>
+	</div>
 </div>
